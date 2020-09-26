4d73cbc169c309bb86caa074b9e916622c766745
damienzhong <damienzhong@foxmail.com>
2020-09-26 23:13:51 +0800

    add ThreadTest

---
+++ src/multithreading/ThreadTest.java
@@ -0,0 +1,15 @@
+package multithreading;
+
+public class ThreadTest extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            if (i % 2 == 0)
+                System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        new ThreadTest().start();
+    }
+}
